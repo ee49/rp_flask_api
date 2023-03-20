061ac8bfef3d423cbce74caec4c8235fb63cc1f5
learn <mithsupatel@gmail.com>
2023-03-19 21:35:49 -0400

    Added SQLAlchemy support and redesigned models to reflect same changes in Marshmallow.
    Signed-off-by: learn <mithunpatellu@gmail.com>

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import render_template
-import connexion
+import config
+from models import Person
 
-app = connexion.App(__name__, specification_dir="/Users/learn/pylearn/rp_flask_api/")
-app.add_api("/Users/learn/pylearn/rp_flask_api/swagger.yml")
+app = config.connex_app
+app.add_api(config.basedir/"swagger.yml")
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    people = Person.query.all()
+    return render_template("home.html", people=people)
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
--- people.py
+++ people.py
@@ -1,69 +1,56 @@
-from datetime import datetime
+
 from flask import abort, make_response
 
-def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+from config import db
+from models import Person, person_schema,people_schema,PersonSchema
 
 def read_all():
-    return list(PEOPLE.values())
+    people = Person.query.all()
+    return people_schema.dump(people)
 
+#deserialize the JSON struction sent from the HTTP to python object and to DB
+# using marshmellow and SqlAlchemy respectively
 def create(person):
     lname=person.get("lname")
-    fname=person.get("fname","")
+    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
 
-    if lname and fname not in PEOPLE:
-        PEOPLE[lname] = {
-            "lname":lname,
-            "fname":fname,
-            "timestamp":get_timestamp()
-        }
-        return PEOPLE[lname], 201
+    if existing_person is None:
+        new_person = person_schema.load(person,session=db.session)
+        db.session.add(new_person)
+        db.session.commit()
+        return person_schema.dump(new_person), 201
     else:
         abort(406,f"Person with last name {lname} already exist",)
 
 def read_one(lname):
-    if lname in PEOPLE:
-        return PEOPLE[lname]
+    #get one person or return None if no match is found
+    person = Person.query.filter(Person.lname == lname).one_or_none()
+    #return the serialized object for the matching name.
+    if person is not None:
+        return person_schema.dump(person)
     else:
         abort(404, f"person with last name {lname} not found")
 
 def update(lname,person):
-    if lname in PEOPLE:
-        PEOPLE[lname]['fname'] = person.get("fname",PEOPLE[lname]["fname"])
-        PEOPLE[lname]["timestamp"] = get_timestamp()
-        return PEOPLE[lname]
+    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+
+    if existing_person:
+        update_person = person_schema.load(person, session=db.session)
+        existing_person.fname = update_person.fname
+        db.session.merge(existing_person)
+        db.session.commit()
+        return person_schema.dump(existing_person), 201
     else:
         abort(404, f"Person with last name {lname} not found")
 
 def delete(lname):
-    if lname in PEOPLE:
-        del PEOPLE[lname]
+    print(f"person to be deleted:{lname}")
+    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+    print(f"Existing person value: {existing_person}")
+    if existing_person:
+        db.session.delete(existing_person)
+        db.session.commit()
         return make_response(f"{lname} successfully deleted", 200)
     else:
         abort(404,f"Person with last name {lname} not found")
-
-
-PEOPLE = {
-    "Fairy": {
-       "fname": "Tooth",
-        "lname": "Fairy",
-        "timestamp": get_timestamp(),
-    },
-    "Ruprecht": {
-        "fname": "Knecht",
-        "lname": "Ruprecht",
-        "timestamp": get_timestamp(),
-    },
-    "Bunny": {
-        "fname": "Easter",
-        "lname": "Bunny",
-        "timestamp": get_timestamp(),
-    },
-    "Patel": {
-        "fname": "Mithun",
-        "lname": "Patel",
-        "timestamp": get_timestamp(),
-    }
-}
-
 
